628bf054f206512a5388668c7fb10036f0825004
Fersy van der Zee <fersyvanderzee@gmail.com>
2022-11-16 19:40:32 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import mysql.connector
+
+
+host = ""
+user_name = ""
+password = ""
+database = ""
+
+db = mysql.connector.connect(
+    host=host,
+    user=user_name,
+    password=password,
+    database=database
+)
+
+
+# Delete db from connector in order to use this.
+def show_db(db):
+    curs = db.cursor()
+    curs.execute("SHOW DATABASES")
+
+    for x in curs:
+        return x
+
+
+def show_tables(db):
+    curs = db.cursor()
+    curs.execute("SHOW TABLES")
+
+    for x in curs:
+        return(x)
+
+
